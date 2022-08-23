7119af61160d43977ce76d43f3fa4a6a1d29a690
Daniel Gonzalez <daniel_gonzalez_sanchez@yahoo.com>
2022-08-23 08:55:22 +0100

    links and card implemented.

---
+++ Home/src/Components/Card.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Card.css';
+import GithubLogo from '../images/github.png';
+import WebsiteLogot from '../images/website.png';
+
+const Card = () => {
+    return (
+        <div className='card-display card-style'>
+            <h1 className='title'>Salus:</h1>
+            <hr></hr>
+            <p className='description'> First steps of a social media platform where users 
+            can connect with each other, send crypto transfers,
+            and buy/sell NFTs.</p>
+            <ul className='icons-list'>
+                <li><a href='https://salus-crypto.com/' target="_blank"> <img src={WebsiteLogot} /></a></li>
+                <li><a href="https://github.com/Daniel-GS-prog/react-eth-payments" target="_blank"><img src={GithubLogo}/></a></li>
+            </ul>
+
+        </div>
+    );
+}
+
+export default Card;
